01aa83da77bf8e0e30ea473b254771136ecf6a71
aryanyash186 <aryanyash186@gmail.com>
2024-05-31 21:53:07 +0600

    fast commit

---
+++ name.js
@@ -0,0 +1,88 @@
+//1
+let carName = "volvo"; 
+console.log(carName);
+
+
+//2
+//let firstName = "John";
+//console.log(firstName);
+//let lastName = "Doe";
+//console.log(lastName);
+//let age = 35;
+//console.log(age);
+
+
+//3
+//x = 10;
+//y = 5;
+//x = x * y;
+//console.log(x);
+
+
+//4
+//let length = 16; 
+//console.log(typeof length);
+//let lastName = "Johnson"; 
+//console.log(typeof last_Name);
+// const x = {
+//   firstName: "John",  
+//   //lastName: "Doe"
+// };
+//console.log(typeof x);
+
+
+
+//5
+ function myFunction() {
+  //alert("Hello World!");
+};
+myFunction();
+
+
+//6
+let person = {
+    naem : "john",
+    age : 50
+};
+//alert(person.naem + 'is' + person.age);
+
+
+//7
+function button() {
+    //alert("hello every one. how are you?");
+}
+button();
+
+
+//8
+const cars = ["Volvo", "Jeep", "Mercedes"];
+//alert(cars.length);
+
+const Brand = ["Volvo", "Jeep", "Mercedes"];
+Brand[0]='Ford';
+console.log(Brand);
+
+
+//9
+let random = Math.random();
+console.log(random);
+
+function large(a,b) {
+    return Math.max(a,b);
+};
+let result = large(10,20); 
+console.log(result);
+
+let root = Math.sqrt(9);
+console.log(root);
+
+
+//10
+x = 10;
+y = 5;
+if (x>y){
+    //alert(true);
+}
+
+let age = 16;
+//let ternary = (age < 18) ? alert("Too young") : alert("Old enough");
